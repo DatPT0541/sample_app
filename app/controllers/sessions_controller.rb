07787c195f61210e1f4db87deeeda9553381a1ae
DatPT0541 <phung.tien.dat@sun-asterisk.com>
2024-07-26 13:23:32 +0700

    Update chapter_10

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,47 @@
 class SessionsController < ApplicationController
+  before_action :find_user_by_email, only: :create
+
   def new; end
 
   def create
-    user = User.find_by email: params.dig(:session, :email)&.downcase
-    if user.try(:authenticate, params.dig(:session, :password))
-      reset_session
-      log_in user
-      params.dig(:session, :remember_me) == "1" ? remember(user) : forget(user)
-      redirect_to user, status: :see_other
+    if user_authenticated? @user
+      handle_authenticated_user @user
     else
-      flash.now[:danger] = t ".message.invalid_email_password_combination"
-      render :new, status: :unprocessable_entity
+      handle_failed_authentication
     end
   end
 
   def destroy
     log_out
     redirect_to root_url, status: :see_other
+  end
+
+  private
+
+  def find_user_by_email
+    @user = User.find_by(email: params.dig(:session, :email)&.downcase)
+    handle_user_not_found if @user.nil?
+  end
+
+  def handle_user_not_found
+    flash.now[:danger] = t ".message.invalid_email_password_combination"
+    render :new, status: :unprocessable_entity
+  end
+
+  def user_authenticated? user
+    user.authenticate(params.dig(:session, :password))
+  end
+
+  def handle_authenticated_user user
+    forwarding_url = session[:forwarding_url]
+    reset_session
+    log_in user
+    params.dig(:session, :remember_me) == "1" ? remember(user) : forget(user)
+    redirect_to forwarding_url || user
+  end
+
+  def handle_failed_authentication
+    flash.now[:danger] = t ".message.invalid_email_password_combination"
+    render :new, status: :unprocessable_entity
   end
 end
