027bee47270bd2c650cdba432f81b7caac2fd205
DatPT0541 <phung.tien.dat@sun-asterisk.com>
2024-07-29 23:55:38 +0700

    Update chapter_12

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,11 @@
 class UserMailer < ApplicationMailer
   def account_activation user
     @user = user
-
-    mail to: user.email, subject: "Account activation"
+    mail to: user.email, subject: t(".message.subject_activation")
   end
 
   def password_reset user
     @user = user
-
-    mail to: user.email, subject: "Reset Password"
+    mail to: user.email, subject: t(".message.subject_reset")
   end
 end
